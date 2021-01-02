ecef0b9467da73dc468090cba4b197ce6b8e70d7
tuncayerman <tuncayermangm@gmail.com>
2021-01-02 11:59:45 +0300

    Order Payment Implemented

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,3 +1,4 @@
+
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema(
@@ -23,6 +24,12 @@ const orderSchema = new mongoose.Schema(
       country: { type: String, required: true },
     },
     paymentMethod: { type: String, required: true },
+    paymentResult: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
+    },
     itemsPrice: { type: Number, required: true },
     shippingPrice: { type: Number, required: true },
     taxPrice: { type: Number, required: true },
